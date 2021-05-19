c107d1616e71c9d08ca9a391c1effa8b5f25f23c
Torben Schinke <torben.schinke@worldiety.de>
2021-05-19 14:20:40 +0200

    added another scheduler test

--- scheduler_test.go
+++ scheduler_test.go
@@ -10,12 +10,30 @@ func TestNewFixedDelayScheduler(t *testing.T) {
 		t.Fatal("should not come here")
 	})
 
-	for i:=0;i<10;i++{
+	for i := 0; i < 10; i++ {
 		go func() {
 			time.Sleep(100)
 			scheduler.Stop()
 		}()
 	}
 
-	time.Sleep(1*time.Second)
+	time.Sleep(1 * time.Second)
+}
+
+func TestNewFixedDelayScheduler2(t *testing.T) {
+	counter := 0
+	var scheduler *FixedDelayScheduler
+
+	scheduler = NewFixedDelayScheduler(100*time.Millisecond, func() {
+		counter++
+		if counter == 5 {
+			scheduler.Stop()
+		}
+	})
+
+	time.Sleep(1 * time.Second)
+
+	if counter != 5 {
+		t.Fatalf("expected 5 but counted %d\n", counter)
+	}
 }
